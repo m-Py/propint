1f7876c61a80ee1af469af1f68db4ac05a24bb8c
m-Py <martin.papenberg@hhu.de>
2016-10-25 11:24:17 +0200

    built package

---
+++ R/propint-package.R
@@ -0,0 +1,22 @@
+#' Interactions in proportions.
+#'
+#' \tabular{ll}{
+#' Package: \tab propint\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.01-1\cr
+#' Date: \tab 2016-10-25\cr
+#' Depends: \tab R (>= 2.10.0)\cr
+#' Encoding: \tab UTF-8\cr
+#' License: \tab GPL (>= 3)\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
+#' Compute confidence intervals for interactions in proportions
+#'
+#' @aliases propint-package
+#' @name propint-package
+#' @docType package
+#' @title The propint Package
+#' @author Martin Papenberg
+#' @keywords package
+NULL
