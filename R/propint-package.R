f9211d193f552448f73dc575e41ccd557c2d07e6
Martin Papenberg <martin.papenberg@hhu.de>
2017-06-08 11:32:39 +0200

    rebuild package

--- R/propint-package.R
+++ R/propint-package.R
@@ -1,10 +1,10 @@
-#' The difference of differences - computing interactions of proportions.
+#' propint - computing interactions of proportions.
 #'
 #' \tabular{ll}{
 #' Package: \tab propint\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.01-1\cr
-#' Date: \tab 2016-10-25\cr
+#' Version: \tab 0.020\cr
+#' Date: \tab 2017-06-08\cr
 #' Depends: \tab R (>= 2.10.0)\cr
 #' Encoding: \tab UTF-8\cr
 #' License: \tab GPL (>= 3)\cr
@@ -12,7 +12,7 @@
 #' URL: \tab https://github.com/m-Py/propint\cr
 #' }
 #'
-#' Computes the confidence interval for interactions in proportions
+#' Computes confidence intervals for interactions in independent and dependent proportions
 #'
 #' @aliases propint-package
 #' @name propint-package
